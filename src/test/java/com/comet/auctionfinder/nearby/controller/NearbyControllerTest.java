1b141dec4fd00a181170f6a07476a7585b313904
choi-hyeseong <bd2845@kakao.com>
2022-10-28 13:58:21 +0900

    테스트 추가

--- src/test/java/com/comet/auctionfinder/nearby/controller/NearbyControllerTest.java
+++ src/test/java/com/comet/auctionfinder/nearby/controller/NearbyControllerTest.java
@@ -1,13 +1,26 @@
 package com.comet.auctionfinder.nearby.controller;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @WebMvcTest(NearbyController.class)
 class NearbyControllerTest {
 
+    @Autowired
+    MockMvc mvc;
+
+
+    @DisplayName("Nearby_Test")
+    @Test
+    void Nearby_Test() throws Exception {
+
+    }
 
 }
